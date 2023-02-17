f7f73f83c173b6211f2d394b474d6ae985301e5b
memsec <zbc.kun@gmail.com>
2023-02-18 02:22:48 +0300

    Editor file ViewController

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -9,9 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    @IBAction func tipChanged(_ sender: UIButton){
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper){
+        
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
         
     }
 
