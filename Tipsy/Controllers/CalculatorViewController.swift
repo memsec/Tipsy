8849ada25687705ee78a529e39f98b99dd377316
memsec <zbc.kun@gmail.com>
2023-02-18 12:47:33 +0300

    Edit file

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -1,9 +1,2 @@
-//
-//  CalculatorViewController.swift
-//  Tipsy
-//
-//  Created by Vladimir Smolyar on 18.02.2023.
-//  Copyright © 2023 The App Brewery. All rights reserved.
-//
 
 import Foundation
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -1,9 +1,2 @@
-//
-//  ResultsViewController.swift
-//  Tipsy
-//
-//  Created by Vladimir Smolyar on 18.02.2023.
-//  Copyright © 2023 The App Brewery. All rights reserved.
-//
 
 import Foundation
