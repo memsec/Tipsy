0293015f70f2888cabc254d732f121905eb419b1
memsec <zbc.kun@gmail.com>
2023-02-18 16:05:08 +0300

    Edit file CalculatorViewController

--- Tipsy/CalculatorViewController.swift
+++ Tipsy/CalculatorViewController.swift
@@ -16,6 +16,7 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func tipChanged(_ sender: UIButton){
         zeroPctButton.isSelected = true
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper){
