799f2d335ff44717b4334ed090907672afeac172
memsec <zbc.kun@gmail.com>
2023-02-18 16:12:41 +0300

    Remove file CalculatorViewController and move folder Controllers

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -1,6 +1,30 @@
 
 import UIKit
 
-//class CalculatorViewController: UIViewController {
-//
-//}
+class CalculatorViewController: UIViewController {
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+
+    var tip = 0.10
+    var numberPeople = 2
+    
+    @IBAction func tipChanged(_ sender: UIButton){
+        zeroPctButton.isSelected = true
+        
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper){
+        
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+    }
+
+
+}
+
